e3ef67114c82ef727cd14c2f8ea3aacb7afb51e4
Vadym Skliarov <sklyarov.vadym@gmail.com>
2015-05-22 14:53:33 +0300

    add attribute weight

--- js/mapYearsGeojson.js
+++ js/mapYearsGeojson.js
@@ -1,4 +1,4 @@
-﻿//map displaying
+//map displaying
 var map = L.map('map').setView([50.4500, 30.5000], 13).setMaxBounds([
     [50.3281, 30.3092],
     [50.5411, 30.7432]]);
@@ -7,17 +7,17 @@ $.getJSON("../PriceForBuildings.geojson", function (data) {
     L.geoJson(data, {
         style: function(building) {
             var price = building.properties.MEDmed;
-            if (price < 800) return { color: "#313695", fillOpacity: 1, opacity: 1, stroke-width: 0};
-            else if (price < 1000) return { color: "#4575B4", fillOpacity: 1, opacity: 1, stroke-width: 0 };
-            else if (price < 1250) return { color: "#74ADD1", fillOpacity: 1, opacity: 1, stroke-width: 0 };
-            else if (price < 1500) return { color: "#ABD9E9", fillOpacity: 1, opacity: 1, stroke-width: 0 };
-            else if (price < 2000) return { color: "#E0F3F8", fillOpacity: 1, opacity: 1, stroke-width: 0 };
-            else if (price < 2500) return { color: "#FFFFBF", fillOpacity: 1, opacity: 1, stroke-width: 0 };
-            else if (price < 3000) return { color: "#FEE090", fillOpacity: 1, opacity: 1, stroke-width: 0 };
-            else if (price < 4000) return { color: "#FDAE61", fillOpacity: 1, opacity: 1, stroke-width: 0 };
-            else if (price < 5000) return { color: "#F46D43", fillOpacity: 1, opacity: 1, stroke-width: 0 };
-            else if (price < 7000) return { color: "#D73027", fillOpacity: 1, opacity: 1, stroke-width: 0 };
-            else if (price > 7000) return { color: "#A50026", fillOpacity: 1, opacity: 1, stroke-width: 0 };
+            if (price < 800) return { color: "#313695", fillOpacity: 1, opacity: 1, weight: 0};
+            else if (price < 1000) return { color: "#4575B4", fillOpacity: 1, opacity: 1, weight: 0 };
+            else if (price < 1250) return { color: "#74ADD1", fillOpacity: 1, opacity: 1, weight: 0 };
+            else if (price < 1500) return { color: "#ABD9E9", fillOpacity: 1, opacity: 1, weight: 0 };
+            else if (price < 2000) return { color: "#E0F3F8", fillOpacity: 1, opacity: 1, weight: 0 };
+            else if (price < 2500) return { color: "#FFFFBF", fillOpacity: 1, opacity: 1, weight: 0 };
+            else if (price < 3000) return { color: "#FEE090", fillOpacity: 1, opacity: 1, weight: 0 };
+            else if (price < 4000) return { color: "#FDAE61", fillOpacity: 1, opacity: 1, weight: 0 };
+            else if (price < 5000) return { color: "#F46D43", fillOpacity: 1, opacity: 1, weight: 0 };
+            else if (price < 7000) return { color: "#D73027", fillOpacity: 1, opacity: 1, weight: 0 };
+            else if (price > 7000) return { color: "#A50026", fillOpacity: 1, opacity: 1, weight: 0 };
             return { color: "#d3d3d3" }
         }
     }).addTo(map);
